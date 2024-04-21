ee9fa21a053dd67dd1358e2a8da0197442427024
Yammz <113056357+ygarciaroc@users.noreply.github.com>
2024-04-21 12:46:54 -0600

    Add files via upload

---
+++ python002.py
@@ -0,0 +1,6 @@
+
+##Find sequare root of Number in Python
+number = int(input("Enter number to find square root: "))
+
+square_root = number ** 0.5 ##exponent symbol
+print("Square root of {} is {}".format(number, square_root))
