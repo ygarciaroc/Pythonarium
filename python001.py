874f0f85b9eccf0f1f971e8dc3e4f5026b6120f7
Yammz <113056357+ygarciaroc@users.noreply.github.com>
2024-04-21 12:52:29 -0600

    Update python001.py

--- python001.py
+++ python001.py
@@ -1,6 +1,17 @@
-## Find Sum of Numbers Entered by User
+## Find Sum of Numbers Entered by User (deactivated by ## symbols)
 
-number1 = int(input("Enter first number "))
-number2 = int(input("Enter second number: "))
+#number1 = int(input("Enter first number "))
+#number2 = int(input("Enter second number: "))
 
-print("The sum of {} and {} is: {}".format(number1, number2, number1 + number2))
+#print("The sum of {} and {} is: {}".format(number1, number2, number1 + number2))
+
+
+## Entering the condition  (active)
+count = int(input("Enter count of numbers : "))
+
+sum = 0
+for num in range(count):
+    number = int(input())
+    sum = sum + number 
+
+print("The sum of all numbers is : ", sum)
