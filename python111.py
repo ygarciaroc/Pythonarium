53fd713276f4c337e590ca85dffad4ee30dff1a2
ygarciaroc <113056357+ygarciaroc@users.noreply.github.com>
2024-04-21 01:02:37 -0600

    Add files via upload

---
+++ python111.py
@@ -0,0 +1,6 @@
+## Find Sum of Numbers Entered by User
+
+number1 = int(input("Enter first number "))
+number2 = int(input("Enter second number: "))
+
+print("The sum of {} and {} is: {}".format(number1, number2, number1 + number2))
